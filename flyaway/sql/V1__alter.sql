d97e78a4d79b4887d5d4daf501c1f697d2905822
jura <jura@expediagroup.com>
2024-07-24 17:31:35 +0200

    adding flyaway

---
+++ flyaway/sql/V1__alter.sql
@@ -0,0 +1,11 @@
+ALTER TABLE schema1.users ADD COLUMN age INT;
+ALTER TABLE schema2.users ADD COLUMN age INT;
+ALTER TABLE schema3.users ADD COLUMN age INT;
+ALTER TABLE schema4.users ADD COLUMN age INT;
+ALTER TABLE schema5.users ADD COLUMN age INT;
+ALTER TABLE schema6.users ADD COLUMN age INT;
+ALTER TABLE schema7.users ADD COLUMN age INT;
+ALTER TABLE schema8.users ADD COLUMN age INT;
+ALTER TABLE schema9.users ADD COLUMN age INT;
+ALTER TABLE schema10.users ADD COLUMN age INT;
+
