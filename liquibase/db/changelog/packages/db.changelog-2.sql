5cce128eeaf933d3e8fd4c1745410dbcb19a8941
jura <jura@expediagroup.com>
2024-07-25 11:16:27 +0200

    fixing changeset

--- liquibase/db/changelog/packages/db.changelog-2.sql
+++ liquibase/db/changelog/packages/db.changelog-2.sql
@@ -3,7 +3,7 @@
 
 
 --changeset jc:1 labels:assignment-test 
-ALTER TABLE schema10.users ADD COLUMN department VARCHAR (255); 
---rollback ALTER TABLE schema10.users DROP COLUMN department;
+ALTER TABLE schema1.users ADD COLUMN department VARCHAR (255); 
+--rollback ALTER TABLE schema1.users DROP COLUMN department;
 
 
